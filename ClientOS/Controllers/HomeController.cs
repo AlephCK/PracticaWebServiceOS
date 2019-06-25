c3f5036327b32029cf159ac5bde7ac718d427e06
Arliin23 <Arliiin23@gmail.com>
2019-06-25 14:41:09 -0400

    ClientOS Finish
    Implementación de la api conseguida

--- ClientOS/Controllers/HomeController.cs
+++ ClientOS/Controllers/HomeController.cs
@@ -5,14 +5,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ClientOS.Models;
+using ClientOS.Helper;
+using WebServiceAppOS.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace ClientOS.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        AcreditacionAPI _api = new AcreditacionAPI();
+
+        public async Task <IActionResult> Index()
         {
-            return View();
+            List<Acreditacion> acreditaciones = new List<Acreditacion>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/Acreditacion");
+
+            if (res.IsSuccessStatusCode)
+            {
+
+                var result = res.Content.ReadAsStringAsync().Result;
+                acreditaciones = JsonConvert.DeserializeObject<List<Acreditacion>>(result);
+            }
+
+
+            return View(acreditaciones);
         }
 
         public IActionResult Privacy()
